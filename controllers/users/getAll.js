ba6a1b6320bc3effd8984732727d8a02526de5b1
aleksandrchuyko <chao.mess@gmail.com>
2023-05-21 22:22:40 +0300

    Update getAll.js

--- controllers/users/getAll.js
+++ controllers/users/getAll.js
@@ -3,13 +3,11 @@ const { User } = require('../../models/users/user');
 const getAll = async (req, res) => {
   const owner = req.owner._id;
 
-  const { page = 1, limit = 5, favorite = false } = req.query;
+  const { page = 1, limit = 100 } = req.query;
   const skip = (page - 1) * limit;
 
   const queryParams = { owner };
-  if (favorite === 'true') {
-    queryParams.favorite = true;
-  }
+
   const result = await User.find(queryParams, '-createdAt -updatedAt', {
     skip,
     limit,
